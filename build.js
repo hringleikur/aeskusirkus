63fc4a20e625b6bc33a46a751fc798e0e0383c6b
Thomas Burke <tom@intelesense.net>
2019-01-24 17:26:45 +0000

    Update to test content creation.

--- build.js
+++ build.js
@@ -1,9 +1,31 @@
 
 const REPO = 'github.com/hringleikur/aeskusirkus';
-const git = require('simple-git/promise');
+const WORKING_BRANCH = 'master';
+const TARGET_BRANCH = 'master';
+const CONTENT_DIR = "content/";
+const CONTENT_SUFFIX = ".md";
+const git = require('simple-git/promise')('../working');
 const remote = `https://${process.env.GITHUB_USERNAME}:${process.env.GITHUB_PASSWORD}@${REPO}`;
 
-git().silent(true)
-  .fetch({"--unshallow":null})
-  .then(() => console.log('finished'))
-  .catch((err) => console.error('failed: ', err));
+git
+  .silent(true)
+  .checkIsRepo()
+  .then(isRepo => isRepo ? git.pull(remote) : git.clone(remote, '.'))
+  .then(r => git.checkout(WORKING_BRANCH))
+  .then(r => git.show(["--pretty=","--name-only"]))
+  .then(checkFiles)
+  .catch(err => console.error('failed: ', err));
+
+
+  function checkFiles(list)
+  {
+    var allFiles = list.split("\n");
+    var contentFiles = allFiles
+        .filter(file=>file.startsWith(CONTENT_DIR) && file.endsWith(CONTENT_SUFFIX))
+    contentFiles.forEach(handleContentFile);
+  }
+
+  function handleContentFile(file)
+  {
+    console.log(file);
+  }
---
+++ content/is/sumarnamskeid/index.md
@@ -0,0 +1,8 @@
+---
+date: "2012-04-06"
+description : "sumarnámskeið"
+layout : "sumarnamskeid"
+title : "sumarnámskeið"
+weight : 2
+translationKey: "sumarnamskeid"
+---
