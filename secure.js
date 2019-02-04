b8b35b66912ff86f783c8f8355914e2dc17de9d2
Thomas Burke <tom@intelesense.net>
2019-02-04 14:46:55 +0000

    front page working mostly

--- secure.js
+++ secure.js
@@ -10,25 +10,19 @@ try
   var password = yaml.safeLoad(fs.readFileSync(CONFIG, 'utf8')).password;
   exec('find ./public/en/members -name "*.html" -exec node ./node_modules/staticrypt/index.js {} "'+password+'" -f ./public/en/auth/index.html -o {}  \\;', (err, stdout, stderr) => {
     if (err) {
-      // node couldn't execute the command
       console.log("Error", err);
       return;
     }
 
-    // the *entire* stdout and stderr (buffered)
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
+    console.log("Done encrypting.")
   });
   exec('find ./public/innri-vefur -name "*.html" -exec node ./node_modules/staticrypt/index.js {} "'+password+'" -f ./public/auth/index.html -o {}  \\;', (err, stdout, stderr) => {
     if (err) {
-      // node couldn't execute the command
       console.log("Error", err);
       return;
     }
 
-    // the *entire* stdout and stderr (buffered)
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
+    console.log("Done encrypting.")
   });
 }
 catch(e)
